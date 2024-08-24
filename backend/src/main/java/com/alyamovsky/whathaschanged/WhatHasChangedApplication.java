9a185b37619a5be262c17c889423243268da92e8
Andrew Alyamovsky <andrew.alyamovsky@gmail.com>
2024-08-24 17:27:22 +0200

    Initial commit

---
+++ backend/src/main/java/com/alyamovsky/whathaschanged/WhatHasChangedApplication.java
@@ -0,0 +1,13 @@
+package com.alyamovsky.whathaschanged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatHasChangedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatHasChangedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alyamovsky/whathaschanged/WhatHasChangedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alyamovsky.whathaschanged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatHasChangedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
