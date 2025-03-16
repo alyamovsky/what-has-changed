48ac268114be04b8ec2bc7221577979ae0368ff4
Andrew Alyamovsky <andrew.alyamovsky@gmail.com>
2025-03-17 00:04:01 +0100

    Fix dotenv

--- backend/src/main/java/com/alyamovsky/whathaschanged/command/AppsRankingUpdateCommand.java
+++ backend/src/main/java/com/alyamovsky/whathaschanged/command/AppsRankingUpdateCommand.java
@@ -1,18 +1,10 @@
 package com.alyamovsky.whathaschanged.command;
 
-import com.alyamovsky.whathaschanged.dto.SteamApp;
-import com.alyamovsky.whathaschanged.dto.SteamAppList;
-import com.alyamovsky.whathaschanged.repository.SteamAppRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestClient;
-
-import java.util.List;
 
 @ShellComponent
 public class AppsRankingUpdateCommand {
